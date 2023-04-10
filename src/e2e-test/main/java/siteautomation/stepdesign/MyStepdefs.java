5ab2621d9bd73647bd9704e89c4e74a75f677ade
ankit yadav <ankityadavjal2627@gmail.com>
2023-04-10 15:59:34 +0530

    file changed

--- src/e2e-test/main/java/siteautomation/stepdesign/MyStepdefs.java
+++ src/e2e-test/main/java/siteautomation/stepdesign/MyStepdefs.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import siteautomation.actions.ActionsClass;
 
-
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
